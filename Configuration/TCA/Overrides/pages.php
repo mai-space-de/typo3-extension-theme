de2658aa77d4a9ec16dbb7751227121432b4a0e0
maispace <joel@maispace.de>
2026-05-30 13:58:11 +0200

    [BUGFIX] Visual Editor: Complete pageTSconfig integration
    - Add pageTSconfig declaration to config.yaml to load BackendLayout TsConfig
    - Set default backend_layout='default' in TCA for new pages
    - Ensure all pages have BackendLayout with colPos 0-6 defined
    
    Fixes: Visual Editor error 'Content area with colPos 1 not found in page layout'

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -8,5 +8,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 $typo3Version = GeneralUtility::makeInstance(Typo3Version::class);
 
 if ($typo3Version->getMajorVersion() >= 13) {
-    $GLOBALS['TCA']['pages']['columns']['backend_layout']['config']['default'] = 'default';
+    // Page-TSconfig layouts live under the "pagets" data provider (pagets__default).
+    // The core "default" provider only exposes colPos 0 and breaks Visual Editor.
+    $GLOBALS['TCA']['pages']['columns']['backend_layout']['config']['default'] = 'pagets__default';
 }
